7a296412b27b4b74942f795f6431bce4deebb30e
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-22 16:38:15 +0200

    Loading bonus

--- src/components/DiscList.vue
+++ src/components/DiscList.vue
@@ -1,27 +1,31 @@
 <template>
-  <main>
-      <div class="container">
-          <div class="row">
-              <div v-for="(disc, index) in discList" :key="index" class="col-6 col-md-4 col-lg-3 mb-5">
-                  <Disc :info= disc />
-              </div>
-          </div>
-      </div>
-  </main>
+    <main>
+        <div class="container">
+            <div class="row" v-if="!loading">
+                <div v-for="(disc, index) in discList" :key="index" class="col-6 col-md-4 col-lg-3 mb-5">
+                    <Disc :info= disc />
+                </div>
+            </div>
+            <Loader v-else />
+        </div>
+    </main>
 </template>
 
 <script>
 import axios from 'axios';
 import Disc from './Disc.vue';
+import Loader from './Loader.vue';
 export default {
     name: "DiscList",
     components: {
-        Disc
+        Disc,
+        Loader
     },
     data() {
         return {
            APIurl: 'https://flynn.boolean.careers/exercises/api/array/music',
-           discList: [] 
+           discList: [],
+           loading: true 
         }
     },
     created() {
@@ -33,7 +37,12 @@ export default {
             .then(res => {
                 console.log(res.data.response);
                 this.discList = res.data.response
+                this.loading = false;
+                setTimeout( () => {this.loading = false; }, 5000);
             })
+            .catch( err => {
+            console.log("Error ", err);
+          })
         }
     }
 }
