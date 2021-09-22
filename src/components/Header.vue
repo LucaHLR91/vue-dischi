73d7a78230904f61681f3a1d44e22e6bcba3e74e
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-22 15:19:17 +0200

    Header

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <div id="logo">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png" alt="Logo Spotify">
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        width: 100%;
+        height: 70px;
+        background-color: #2E3A46;
+
+        #logo {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 50px;
+            height: 100%;
+            margin-left: 20px;
+
+            img {
+                width: 100%;
+            }
+        }
+    }
+</style>
